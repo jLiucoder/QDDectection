f89eefa08d6e9e64a93d6393d0b24629f0758740
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2021-07-29 11:41:13 -0600

    Add files via upload

--- labelingSession&TrianingScripts/HOGDetectImage.m
+++ labelingSession&TrianingScripts/HOGDetectImage.m
@@ -1,15 +1,16 @@
 %% select XML file for dectection
-    detector = vision.CascadeObjectDetector('QD_2_5.xml');
+   detector = vision.CascadeObjectDetector('QD_2_5.xml');
     
 %%Load input
-    load 'C:\Users\Jiayu\iCloudDrive\Documents\BSU\2021summerResearch\Positive and Negative dataset\positiveFor.5&1\156_1b.tif';
+   image = imread('Positive and Negative dataset\positiveFor.5&1\156_1b.tif');
+    
     
 %%Find the Bounding box
-    bbox = step(detector,I);
+    bbox = step(detector,image);
 
 %% mark the location on the image using a bounding box
-    J = insertShape(I,'Rectangle',bbox);
-    iamshow(J);
+    J = insertShape(image,'Rectangle',bbox);
+    imshow(J);
  
 %%clean up
    release(detector);
